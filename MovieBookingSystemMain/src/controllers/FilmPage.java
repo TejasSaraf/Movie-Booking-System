3d7255c93652aea40615aeaaae6e05e08a4e44b6
TejasSaraf <saraftejas12@gmail.com>
2024-11-26 11:44:16 -0800

    Added functionality to delete the Film from database

--- MovieBookingSystemMain/src/controllers/FilmPage.java
+++ MovieBookingSystemMain/src/controllers/FilmPage.java
@@ -1,17 +1,25 @@
 package controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Stage;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.text.Text;
 import models.DBConnect;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.io.ByteArrayInputStream;
 
 public class FilmPage {
 	@FXML
@@ -30,10 +38,14 @@ public class FilmPage {
 	private Label lblImdbRating;
 	@FXML
 	private Label lblTimes;
+	@FXML
+	private Button btnDelete;
 
 	private final DBConnect dbConnect = new DBConnect();
+	private int currentFilmId;
 
 	public void setImageId(int imageId) {
+		currentFilmId = imageId;
 		loadFilmDetails(imageId);
 	}
 
@@ -57,30 +69,63 @@ public class FilmPage {
 				}
 
 				// Load text data
-				String title = resultSet.getString("title");
-				lblTitle.setText(title != null ? title : "");
-
-				String description = resultSet.getString("description");
-				txtdescription.setText(description != null ? description : "");
-
-				java.sql.Date dateFrom = resultSet.getDate("date_from");
-				lblFrom.setText(dateFrom != null ? dateFrom.toString() : "");
-
-				java.sql.Date dateTo = resultSet.getDate("date_to");
-				lblTo.setText(dateTo != null ? dateTo.toString() : "");
-
-				String ageRating = resultSet.getString("age_rating");
-				lblAgeRating.setText(ageRating != null ? ageRating : "");
-
-				String imdbRating = resultSet.getString("imdb_rating");
-				lblImdbRating.setText(imdbRating != null ? imdbRating : "");
-
-				String times = resultSet.getString("times");
-				lblTimes.setText(times != null ? times : "");
+				lblTitle.setText(resultSet.getString("title"));
+				txtdescription.setText(resultSet.getString("description"));
+				lblFrom.setText(resultSet.getDate("date_from").toString());
+				lblTo.setText(resultSet.getDate("date_to").toString());
+				lblAgeRating.setText(resultSet.getString("age_rating"));
+				lblImdbRating.setText(resultSet.getString("imdb_rating"));
+				lblTimes.setText(resultSet.getString("times"));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 			System.out.println("Error retrieving film details from database: " + e.getMessage());
 		}
+	}
+
+	@FXML
+	private void handleDeleteFilm() {
+		if (currentFilmId == 0) {
+			showAlert(Alert.AlertType.ERROR, "No film selected", "Please select a film to delete.");
+			return;
+		}
+
+		String deleteQuery = "DELETE FROM films WHERE id = ?";
+
+		try (Connection connection = dbConnect.connect();
+				PreparedStatement statement = connection.prepareStatement(deleteQuery)) {
+
+			statement.setInt(1, currentFilmId);
+
+			int rowsAffected = statement.executeUpdate();
+			if (rowsAffected > 0) {
+				showAlert(Alert.AlertType.INFORMATION, "Film Deleted",
+						"The selected film has been deleted successfully.");
+				redirectToViewFilms();
+			} else {
+				showAlert(Alert.AlertType.ERROR, "Deletion Failed", "Could not delete the film. Please try again.");
+			}
+
+		} catch (SQLException | IOException e) {
+			e.printStackTrace();
+			System.out.println("Error deleting film from database: " + e.getMessage());
+		}
+	}
+
+	private void redirectToViewFilms() throws IOException {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/ViewFilms.fxml"));
+		Scene scene = new Scene(loader.load());
+
+		// Get the current stage and set the new scene
+		Stage stage = (Stage) btnDelete.getScene().getWindow();
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	private void showAlert(Alert.AlertType alertType, String title, String message) {
+		Alert alert = new Alert(alertType, message, ButtonType.OK);
+		alert.setTitle(title);
+		alert.setHeaderText(null);
+		alert.showAndWait();
 	}
 }
